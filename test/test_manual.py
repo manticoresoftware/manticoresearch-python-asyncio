88bbb47ca7ab7d4dbec6b14e3e0654c184428883
nick <nick@manticoresearch.com>
2025-03-28 23:52:06 +0700

    Updated tests

--- test/test_manual.py
+++ test/test_manual.py
@@ -29,6 +29,12 @@ class TestManualApi(IsolatedAsyncioTestCase):
         
     async def test_manual(self):
         async with manticoresearch.ApiClient(self.configuration) as client:
+            indexApi = manticoresearch.IndexApi(client)
+            utilsApi = manticoresearch.UtilsApi(client)
+            searchApi = manticoresearch.SearchApi(client)
+        
+            await utilsApi.sql('query=DROP TABLE IF EXISTS movies')
+            res = await utilsApi.sql("CREATE TABLE IF NOT EXISTS movies (title text, plot text, _year integer, rating float, code multi) min_infix_len='2'")
             pprint("Tests finished")
         
 if __name__ == '__main__':
